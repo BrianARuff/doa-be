a84836f2167e1153ff9810b7ded6bff379d5f7b3
BrianARuff <thelonewolf198927@gmail.com>
2018-12-13 22:46:40 -0500

    magic

--- knexfile.js
+++ knexfile.js
@@ -12,15 +12,16 @@ module.exports = {
       seeds: { directory: "./data/seeds" }
   },
 
-  // production: {
-  //   client: 'pg',
-  //   connection: process.env.DATABASE_URL,
-  //   migrations: {
-  //     directory: './data/migrations'
-  //   },
-  //   seeds: {
-  //     directory: './data/seeds/'
-  //   },
-  //   useNullAsDefault: true
-  // }
+  production: {
+    client: "sqlite3",
+      connection: {
+          filename: "./data/dead_or_alive.sqlite3"
+      },
+      useNullAsDefault: true, // used to avoid warning on console
+      migrations: {
+          directory: "./data/migrations",
+          tableName: "dbmigrations"
+      },
+      seeds: { directory: "./data/seeds" }
+  }
 };
